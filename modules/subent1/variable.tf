b8e4255e249875e36c54d60779122471767043d1
sushmithan <sushmithan@sysgain.com>
2018-07-12 12:15:42 +0530

    Create variable.tf

---
+++ modules/subent1/variable.tf
@@ -0,0 +1,9 @@
+variable virtual_network_name {
+  default = ""
+  description = "Virtual network name"
+}
+variable resource_group_name {
+  default = ""
+  description = "Resource group name"
+}
+
