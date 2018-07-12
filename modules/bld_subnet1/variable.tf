789d8799ec7e39254eac0f0c98aa4d71a3954c96
sushmithan <sushmithan@sysgain.com>
2018-07-12 12:03:11 +0530

    Create variable.tf

---
+++ modules/bld_subnet1/variable.tf
@@ -0,0 +1,12 @@
+variable virtual_network_name {
+  default = ""
+  description = "Virtual network name"
+}
+variable resource_group_name {
+  default = ""
+  description = "Resource group name"
+}
+
+variable network_security_group_id {
+  default = ""
+}
