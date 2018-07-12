e026a0cb22a4a53387f3a0f1695a226535520ffe
sushmithan <sushmithan@sysgain.com>
2018-07-12 12:06:33 +0530

    Create main.tf

---
+++ modules/bld02/main.tf
@@ -0,0 +1,60 @@
+resource "azurerm_network_interface" "bldnetworkInterface02" {
+name                      = "${var.resource_group_name}-BLDNIC02"
+location                  = "${var.location}"
+resource_group_name       = "${var.resource_group_name}"
+network_security_group_id = "${var.network_security_group_id}"
+
+ip_configuration {
+      name              = "configuration1"
+      subnet_id         = "${var.subnet_id}"
+      private_ip_address_allocation = "static"
+      private_ip_address = "10.14.10.4"
+   }
+}
+resource "azurerm_managed_disk" "rdbmanagedisk2" {
+    name                 = "BLD02DATADISK02"
+    location             = "${var.location}"
+    resource_group_name  = "${var.resource_group_name}"
+    storage_account_type = "Standard_LRS"
+    create_option        = "Empty"
+    disk_size_gb         = "1023"
+    }
+resource "azurerm_virtual_machine" "bldvirtualmachine02" {
+    name                = "${var.resource_group_name}BLD02"
+    location            = "${var.location}"
+    resource_group_name = "${var.resource_group_name}"
+    network_interface_ids = ["${var.network_interface_ids}"]
+    vm_size             = "${var.bldvmSize}"
+
+storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+    }
+storage_os_disk {
+    name              = "${var.resource_group_name}BLDOSDisk2"
+    managed_disk_type = "Standard_LRS"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+}
+storage_data_disk {
+    name            = "${azurerm_managed_disk.rdbmanagedisk2.name}"
+    managed_disk_id = "${azurerm_managed_disk.rdbmanagedisk2.id}"
+    disk_size_gb    = "1023"
+    create_option   = "Attach"
+    lun             = 1
+    }
+os_profile {
+    computer_name  =  "EU1PRDBLD02"
+    admin_username = "${var.admin_username}"
+    admin_password = "${var.admin_password}"
+}
+os_profile_linux_config {
+disable_password_authentication = false
+}
+
+tags {
+environment = "production"
+}
+}
